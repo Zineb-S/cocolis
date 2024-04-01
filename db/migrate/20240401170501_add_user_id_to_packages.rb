245588140bf1961332ca6af25981927be23930e6
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2024-04-01 17:57:20 +0000

    deed

--- db/migrate/20240401170501_add_user_id_to_packages.rb
+++ db/migrate/20240401170501_add_user_id_to_packages.rb
@@ -1,10 +1,7 @@
 class AddUserIdToPackages < ActiveRecord::Migration[7.1]
   def change
-    add_reference :packages, :user, null: false, foreign_key: true, default: default_user_id
+    add_reference :packages, :user, null: false, foreign_key: true
   end
 
-  def default_user_id
-    User.first.id
-  end
 
 end
