bae97d28a4893ff92050094eeca5c940172967d2
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2024-04-01 18:18:38 +0000

    Update 20240401170518_add_client_and_driver_to_deliveries.rb

--- db/migrate/20240401170518_add_client_and_driver_to_deliveries.rb
+++ db/migrate/20240401170518_add_client_and_driver_to_deliveries.rb
@@ -1,5 +1,5 @@
 class AddClientAndDriverToDeliveries < ActiveRecord::Migration[7.1]
   def change
-    add_reference :deliveries, :driver, foreign_key: { to_table: :users }
+    #  add_reference :deliveries, :driver, foreign_key: { to_table: :users }
   end
 end
