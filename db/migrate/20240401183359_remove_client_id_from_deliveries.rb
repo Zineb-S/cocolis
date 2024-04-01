2fdd66035eff2f07c5362d6bd0987e8c8c09aa65
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2024-04-01 18:40:51 +0000

    delete

--- db/migrate/20240401183359_remove_client_id_from_deliveries.rb
+++
@@ -1,5 +0,0 @@
-class RemoveClientIdFromDeliveries < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :deliveries, :client_id, :integer
-  end
-end
--- db/migrate/20240401183404_remove_client_email_from_deliveries.rb
+++
@@ -1,5 +0,0 @@
-class RemoveClientEmailFromDeliveries < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :deliveries, :client_email, :string
-  end
-end
