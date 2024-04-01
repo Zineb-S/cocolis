74e72a2f4188db4d101747741bf31726440b4a93
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2024-04-01 16:40:20 +0000

    hopeeeeeeee

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,23 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# Drop the existing database
+bundle exec rake db:drop
+
+# Create the database
+bundle exec rake db:create
+
+# Run migrations
 bundle exec rake db:migrate
+
+# Seed the database
 bundle exec rake db:seed
+
+# Install dependencies
+bundle install
+
+# Precompile assets
+bundle exec rake assets:precompile
+
+# Clean up old assets
+bundle exec rake assets:clean
