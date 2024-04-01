be2f24ebdd637bfb59b78946407a8838d64f9b5f
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2024-04-01 18:05:09 +0000

    Update 20240401170501_add_user_id_to_packages.rb

--- db/migrate/20240401170501_add_user_id_to_packages.rb
+++ db/migrate/20240401170501_add_user_id_to_packages.rb
@@ -1,7 +1,12 @@
 class AddUserIdToPackages < ActiveRecord::Migration[7.1]
   def change
-    add_reference :packages, :user, null: true, foreign_key: true
+    # Create default user if it doesn't exist
+    default_user = User.find_or_create_by!(email: 'default@example.com') do |user|
+      user.password = 'defaultpassword'
+      # set other required fields as necessary
+    end
+
+    # Then you can safely use the id
+    add_reference :packages, :user, null: false, foreign_key: true, default: default_user.id
   end
-
-
 end
