7ad786707a94f1475998deb42ae7bf37caa74dcb
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2024-04-01 18:07:20 +0000

    Update 20240401170501_add_user_id_to_packages.rb

--- db/migrate/20240401170501_add_user_id_to_packages.rb
+++ db/migrate/20240401170501_add_user_id_to_packages.rb
@@ -1,9 +1,13 @@
 class AddUserIdToPackages < ActiveRecord::Migration[7.1]
   def change
-    # Create default user if it doesn't exist
+    # Create default user with all necessary fields filled
     default_user = User.find_or_create_by!(email: 'default@example.com') do |user|
       user.password = 'defaultpassword'
-      # set other required fields as necessary
+      user.first_name = 'Default'
+      user.last_name = 'User'
+      user.address = '123 Default St'
+      user.credit_card_info = '4111111111111111' # Use a dummy number that passes validation
+      # Add any other required fields here
     end
 
     # Then you can safely use the id
